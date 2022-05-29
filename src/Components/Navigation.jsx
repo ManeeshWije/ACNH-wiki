5c3950171d661794fb3e6ce3a8a40224dfe54133
ManeeshWije <m.mwije1@gmail.com>
2022-05-29 03:00:36 -0400

    last min changes then actually finish landing page

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -7,59 +7,53 @@ import Nav from "react-bootstrap/Nav";
 
 function Navigation() {
   return (
-    <div className="flex content-center self-center justify-center align-center">
-      <Navbar>
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="w-full px-10 space-x-20 font-['FinkHeavy']">
-              <Nav.Link className="hover:opacity-75 transition-opacity">
+    <>
+      <Nav className="font-['FinkHeavy'] space-x-12 justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link className="text-black hover:opacity-75 transition-opacity" href="/">
+            <img src={require("../assets/paw.png")} alt="paw" />
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <NavDropdown
+            title={
+              <div className="hover:opacity-75 transition-opacity">
                 <img src={require("../assets/paw.png")} alt="paw" />
-                <Link to="/">Home</Link>
-              </Nav.Link>
-              <NavDropdown
-                className="hover:opacity-75 transition-opacity"
-                title={
-                  <>
-                    {" "}
-                    <img src={require("../assets/paw.png")} alt="paw" />
-                    <p>Characters</p>
-                  </>
-                }
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item>
-                  <Link to="/npc">NPC's</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <Link to="/villagers">Villagers</Link>
-                </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown
-                className="hover:opacity-75 transition-opacity"
-                title={
-                  <>
-                    {" "}
-                    <img src={require("../assets/paw.png")} alt="paw" /> <p>Museum</p>
-                  </>
-                }
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="">Bugs</NavDropdown.Item>
-                <NavDropdown.Item href="">Fish</NavDropdown.Item>
-                <NavDropdown.Item href="">Sea Creatures</NavDropdown.Item>
-                <NavDropdown.Item href="">Fossils</NavDropdown.Item>
-                <NavDropdown.Item href="">Art</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link className="hover:opacity-75 transition-opacity" >
+                Characters
+              </div>
+            }
+            id="basic-nav-dropdown"
+          >
+            <NavDropdown.Item href="/npcs">NPC's</NavDropdown.Item>
+            <NavDropdown.Item href="/villagers">Villagers</NavDropdown.Item>
+          </NavDropdown>
+        </Nav.Item>
+        <Nav.Item>
+          <NavDropdown
+            title={
+              <div className="hover:opacity-75 transition-opacity">
                 <img src={require("../assets/paw.png")} alt="paw" />
-                <Link to="/about">About</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+                Museum
+              </div>
+            }
+            id="basic-nav-dropdown"
+          >
+            <NavDropdown.Item href="/bugs">Bugs</NavDropdown.Item>
+            <NavDropdown.Item href="/fish">Fish</NavDropdown.Item>
+            <NavDropdown.Item href="/seaCreatures">Sea Creatures</NavDropdown.Item>
+            <NavDropdown.Item href="/fossils">Fossils</NavDropdown.Item>
+            <NavDropdown.Item href="/art">Art</NavDropdown.Item>
+          </NavDropdown>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className="text-black hover:opacity-75 transition-opacity" href="/about">
+            <img src={require("../assets/paw.png")} alt="paw" />
+            About
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
   );
 }
 
