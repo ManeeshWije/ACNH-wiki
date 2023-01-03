086bb92db0c23c8c395e80cae4a4d41764da7829
ManeeshWije <m.mwije1@gmail.com>
2023-01-03 17:48:51 -0500

    migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+const withMT = require('@material-tailwind/react/utils/withMT');
+module.exports = withMT({
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/Components/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        finkHeavy: ["FinkHeavy", "sans-serif"],
+        fink: ['var(--font-fink)', ...fontFamily.sans],
+        body: ['"Poppins"'],
+      },
+      brightness: {
+        25: '.25',
       },
     },
   },
   plugins: [],
-};
+});
